038ffdde8cbfa6b133360356c8c5f87cd27b4c55
r_wsl <liuhengzhou2416@outlook.com>
2023-11-17 18:34:32 +1100

    sync

---
+++ ass2_submited/RoadMap.c
@@ -0,0 +1,332 @@
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "RoadMap.h"
+#include "time.h"
+
+struct roadMap {
+	// TODO
+	int nN;      // number of nodes
+	int nR;      // number of roads
+	int nI;      // number of islands
+	struct road **roads; // array of roads
+	bool *isTwoWay; // array of booleans to indicate if road is two-way
+	int *islandIds;    // array to store island ID for each node
+    char **islandNames;    // array to store names of the islands
+	bool *trafficLights; // array to store traffic light nodes
+};
+
+static bool checkMaxRoads(RoadMap map, int node);
+static bool checkRoadExists(RoadMap map, int node1, int node2);
+static void addRoad(RoadMap map, int node1, int node2, bool isTwoWay, int travelMinutes);
+static void DFS(RoadMap map, int node, int islandID);
+static int findRoad(RoadMap map, int node1, int node2);
+
+////////////////////////////////////////////////////////////////////////
+// Task 1
+
+RoadMap RoadMapNew(int numNodes) {
+	// TODO
+	if (numNodes < 0) {
+		fprintf(stderr, "error: invalid number of nodes\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// initialise map
+	RoadMap map = malloc(sizeof(*map));
+	if (map == NULL) {
+		fprintf(stderr, "error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	map->nN = numNodes;
+	map->nR = 0;
+	map->nI = 0;
+	map->roads = NULL;
+	map->isTwoWay = NULL;
+	map->islandIds = NULL;
+	map->islandNames = NULL;
+	map->trafficLights = malloc(sizeof(int) * numNodes);
+	if (map->trafficLights == NULL) {
+		fprintf(stderr, "error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 0; i < numNodes; i++) {
+		map->trafficLights[i] = false;
+	}
+
+	return map;
+}
+
+void RoadMapFree(RoadMap map) {
+	// TODO
+	for (int i = 0; i < map->nR; i++) {
+		free(map->roads[i]);
+	}
+	for (int i = 0; i < map->nI; i++) {
+		free(map->islandNames[i]);
+	}
+	free(map->roads);
+	free(map->isTwoWay);
+	free(map->islandIds);
+	free(map->islandNames);
+	free(map->trafficLights);
+	free(map);
+}
+
+int RoadMapNumNodes(RoadMap map) {
+	// TODO
+	return map->nN;
+}
+
+int RoadMapNumRoads(RoadMap map) {
+	// TODO
+	return map->nR;
+}
+
+bool RoadMapAddRoad(RoadMap map, int node1, int node2,
+                    bool isTwoWay, int travelMinutes) {
+    // TODO
+	if (map->roads == NULL) {
+		// add first road
+		addRoad(map, node1, node2, isTwoWay, travelMinutes);
+    } else {
+        // check if road already exists and if node has max roads
+        if (checkRoadExists(map, node1, node2) || !checkMaxRoads(map, node1) ||
+            !checkMaxRoads(map, node2)) {
+            return false;
+        }
+		// add road
+		addRoad(map, node1, node2, isTwoWay, travelMinutes);
+	}
+    return true;
+}
+
+int RoadMapGetRoadsFrom(RoadMap map, int node, struct road roads[]) {
+    // TODO
+	int numRoads = 0;
+	for (int i = 0; i < map->nR; i++) {
+		if (map->roads[i]->fromNode == node) {
+			roads[numRoads] = *(map->roads[i]);
+			numRoads++;
+		} else if (map->roads[i]->toNode == node && map->isTwoWay[i]) {
+			roads[numRoads].fromNode = node;
+			roads[numRoads].toNode = map->roads[i]->fromNode;
+			roads[numRoads].travelMinutes = map->roads[i]->travelMinutes;
+			numRoads++;
+		}
+	}
+    return numRoads;
+}
+
+int RoadMapGetRoadsTo(RoadMap map, int node, struct road roads[]) {
+	// TODO
+	int numRoads = 0;
+	for (int i = 0; i < map->nR; i++) {
+		if (map->roads[i]->toNode == node) {
+			roads[numRoads] = *(map->roads[i]);
+			numRoads++;
+		} else if (map->roads[i]->fromNode == node && map->isTwoWay[i]) {
+			roads[numRoads].fromNode = map->roads[i]->toNode;
+			roads[numRoads].toNode = node;
+			roads[numRoads].travelMinutes = map->roads[i]->travelMinutes;
+			numRoads++;
+		}
+	}
+
+	/*
+	printf("node %d has %d roads connected\n", node, numRoads);
+	for (int i = 0; i < numRoads; i++) {
+		printf("road %d: %d -> %d\n", i, roads[i].fromNode, roads[i].toNode);
+	}
+	*/
+
+    return numRoads;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Task 2
+
+void RoadMapProcessIslands(RoadMap map) {
+	// TODO
+	// Initialize islandIds to -1, indicating unvisited nodes
+    map->islandIds = malloc(sizeof(int) * map->nN);
+    for (int i = 0; i < map->nN; i++) {
+        map->islandIds[i] = -1;
+    }
+
+    int islandID = 0;
+
+    // Run DFS for each node
+    for (int i = 0; i < map->nN; i++) {
+        if (map->islandIds[i] == -1) {
+            DFS(map, i, islandID);
+            islandID++;
+        }
+    }
+
+	// Initialize islandNames to "(unnamed)"
+	map->islandNames = malloc(sizeof(char *) * islandID);
+	for (int i = 0; i < islandID; i++) {
+		map->islandNames[i] = malloc(sizeof(char) * 10);
+		strcpy(map->islandNames[i], "(unnamed)");
+	}
+
+    // Store the number of islands
+    map->nI = islandID;
+
+}
+
+int RoadMapNumIslands(RoadMap map) {
+	// TODO
+	return map->nI;
+}
+
+bool RoadMapOnSameIsland(RoadMap map, int node1, int node2) {
+	// TODO
+	if (map->islandIds[node1] == map->islandIds[node2]) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void RoadMapSetIslandName(RoadMap map, int node, char *name) {
+	// TODO
+	int islandID = map->islandIds[node];
+	strcpy(map->islandNames[islandID], name);
+}
+
+char *RoadMapGetIslandName(RoadMap map, int node) {
+	// TODO
+	int islandID = map->islandIds[node];
+	return map->islandNames[islandID];
+}
+
+////////////////////////////////////////////////////////////////////////
+// Task 3
+
+void RoadMapAddTrafficLights(RoadMap map, int node) {
+	// TODO
+	map->trafficLights[node] = true;
+}
+
+bool RoadMapHasTrafficLights(RoadMap map, int node) {
+	// TODO
+	if (map->trafficLights[node]) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+// Task 4
+
+void RoadMapSetClosedTimes(RoadMap map, int node1, int node2,
+                           struct time from, struct time until) {
+	// TODO
+	int roadIndex = findRoad(map, node1, node2);
+	if (roadIndex == -1) {
+		fprintf(stderr, "error: road not found\n");
+		exit(EXIT_FAILURE);
+	}
+	map->roads[roadIndex]->closedFrom = from;
+	map->roads[roadIndex]->closedUntil = until;
+}
+
+////////////////////////////////////////////////////////////////////////
+// My helper functions
+
+// check if node has max roads
+// returns true if node has less than max roads, false otherwise
+static bool checkMaxRoads(RoadMap map, int node) {
+	int nR = 0;
+	for (int i = 0; i < map->nR; i++) {
+		if (map->roads[i]->fromNode == node || map->roads[i]->toNode == node) {
+			nR++;
+		}
+	}
+	if (nR == MAX_ROADS_PER_NODE) {
+		return false;
+	}
+	//printf("//checking node %d has %d roads connected\n", node, nR);
+	return true;
+}
+
+// check if road is already in map, regardless of direction
+// returns true if road exists, false otherwise
+static bool checkRoadExists(RoadMap map, int node1, int node2) {
+	for (int i = 0; i < map->nR; i++) {
+		if ((map->roads[i]->fromNode == node1 && map->roads[i]->toNode == node2) || 
+			(map->roads[i]->fromNode == node2 && map->roads[i]->toNode == node1)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// add road to map
+static void addRoad(RoadMap map, int node1, int node2, bool isTwoWay, int travelMinutes) {
+	map->nR++;
+	map->roads = realloc(map->roads, map->nR * sizeof(*(map->roads)));
+	if (map->roads == NULL) {
+		fprintf(stderr, "error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	map->roads[map->nR - 1] = malloc(sizeof(struct road));
+	if (map->roads[map->nR - 1] == NULL) {
+		fprintf(stderr, "error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	map->roads[map->nR - 1]->fromNode = node1;
+	map->roads[map->nR - 1]->toNode = node2;
+	map->roads[map->nR - 1]->travelMinutes = travelMinutes;
+	map->isTwoWay = realloc(map->isTwoWay, map->nR * sizeof(bool));
+	if (map->isTwoWay == NULL) {
+		fprintf(stderr, "error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	map->isTwoWay[map->nR - 1] = isTwoWay ? true : false;
+
+	// for task 4
+	map->roads[map->nR - 1]->closedFrom.hour = -1;
+	map->roads[map->nR - 1]->closedFrom.minute = -1;
+	map->roads[map->nR - 1]->closedUntil.hour = -1;
+	map->roads[map->nR - 1]->closedUntil.minute = -1;
+}
+
+// DFS to find islands
+static void DFS(RoadMap map, int node, int islandID) {
+	map->islandIds[node] = islandID;
+	struct road roads[MAX_ROADS_PER_NODE];
+	int numRoads = RoadMapGetRoadsFrom(map, node, roads);
+	for (int i = 0; i < numRoads; i++) {
+		if (map->islandIds[roads[i].toNode] == -1) {
+			DFS(map, roads[i].toNode, islandID);
+		}
+	}
+}
+
+// find road in map
+// returns index of road in map, -1 if road not found
+static int findRoad(RoadMap map, int node1, int node2) {
+    for (int i = 0; i < map->nR; i++) {
+        // is two-way road
+        if (map->isTwoWay[i]) {
+            if ((map->roads[i]->fromNode == node1 && map->roads[i]->toNode == node2) ||
+				(map->roads[i]->fromNode == node2 && map->roads[i]->toNode == node1)) {
+				return i;
+			}
+        } else {
+			if (map->roads[i]->fromNode == node1 && map->roads[i]->toNode == node2) {
+				return i;
+			}
+        }
+    }
+    return -1;
+}
---
+++ ass2_submited/navigator.c
@@ -0,0 +1,420 @@
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "RoadMap.h"
+#include "navigator.h"
+#include "time.h"
+
+#define MINUTES_PER_HOUR 60
+
+static bool findShortestPath(RoadMap map, int fromNode, int toNode, int *dist,
+                             int *prev, bool *visited);
+static void getRoute(RoadMap map, int fromNode, int toNode, struct time time,
+                     int *dist, int *prev, bool *visited, struct route *route);
+static int minDist(int dist[], bool visited[], int numNodes);
+static int RoadMapGetTravelTime(RoadMap map, int fromNode, int toNode);
+static struct time TimeAddMinutes(struct time time, int minutes);
+static int compareTime(struct time t1, struct time t2);
+static int minDistance(struct time dist[], bool visited[], int numNodes);
+static bool RoadMapIsClosed(RoadMap map, struct road road, struct time time);
+static int getMinutes(struct time start, struct time end);
+
+////////////////////////////////////////////////////////////////////////
+// Task 3
+
+struct route navigate(RoadMap map, int fromNode, int toNode, struct time time) {
+    struct route route = {
+        .fromNode = fromNode,
+        .toNode = toNode,
+        .numLegs = 0,
+        .legs = NULL,
+    };
+
+    // use djikstra's algorithm to find shortest path
+    // initialise
+    int *dist = malloc(sizeof(int) * RoadMapNumNodes(map));
+    int *prev = malloc(sizeof(int) * RoadMapNumNodes(map));
+    bool *visited = malloc(sizeof(bool) * RoadMapNumNodes(map));
+    if (dist == NULL || prev == NULL || visited == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < RoadMapNumNodes(map); i++) {
+        dist[i] = INT_MAX;
+        prev[i] = -1;
+        visited[i] = false;
+    }
+
+    // find shortest path
+    bool isPath = findShortestPath(map, fromNode, toNode, dist, prev, visited);
+
+    // Reconstruct and store the path, if there is path
+    if (isPath) {
+        getRoute(map, fromNode, toNode, time, dist, prev, visited, &route);
+    }
+
+    free(dist);
+    free(prev);
+    free(visited);
+    return route;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Task 4
+
+struct route advancedNavigate(RoadMap map, int fromNode, int toNode,
+                              struct time time) {
+    struct route route = {
+        .fromNode = fromNode,
+        .toNode = toNode,
+        .numLegs = 0,
+        .legs = NULL,
+    };
+
+    int numNodes = RoadMapNumNodes(map);
+    // Distance values are now time values.
+    struct time *dist = malloc(sizeof(struct time) * numNodes);
+    int *prev = malloc(sizeof(int) * numNodes);
+    bool *visited = malloc(sizeof(bool) * numNodes);
+    if (dist == NULL || prev == NULL || visited == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize distances as infinity and visited array as false
+    for (int i = 0; i < numNodes; i++) {
+        dist[i] = (struct time){.hour = 99,
+                                .minute = 99}; // Assuming 24:60 as infinity
+        prev[i] = -1;
+        visited[i] = false;
+    }
+
+    // Distance to source is the departure time
+    dist[fromNode] = time;
+    prev[fromNode] = fromNode;
+
+    int u = fromNode;
+    for (int count = 0; count < numNodes - 1 && u != -1; count++) {
+
+        // Mark the picked vertex as processed
+        visited[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex
+        struct road *fromRoads =
+            malloc(sizeof(struct road) * MAX_ROADS_PER_NODE);
+        if (fromRoads == NULL) {
+            fprintf(stderr, "error: out of memory\n");
+            exit(EXIT_FAILURE);
+        }
+        int numFromRoads = RoadMapGetRoadsFrom(map, u, fromRoads);
+        for (int i = 0; i < numFromRoads; i++) {
+            if (visited[fromRoads[i].toNode]) {
+                continue;
+            }
+
+            struct time travelTime = dist[u];
+
+            if (RoadMapHasTrafficLights(map, fromRoads[i].toNode) &&
+                fromRoads[i].toNode != toNode) {
+                fromRoads[i].travelMinutes += TRAFFIC_LIGHT_WAIT;
+            }
+
+            travelTime = TimeAddMinutes(dist[u], fromRoads[i].travelMinutes);
+
+            // Check for road closure and wait until it opens
+            struct time roadOpenTime = fromRoads[i].closedUntil;
+            if (RoadMapIsClosed(map, fromRoads[i], travelTime)) {
+                travelTime = roadOpenTime;
+                travelTime =
+                    TimeAddMinutes(travelTime, fromRoads[i].travelMinutes);
+            }
+
+            if (compareTime(travelTime, dist[fromRoads[i].toNode]) < 0) {
+                dist[fromRoads[i].toNode] = travelTime;
+                prev[fromRoads[i].toNode] = u;
+            }
+        }
+        free(fromRoads);
+        // Find the minimum distance node from the set of vertices not yet
+        // processed
+        u = minDistance(dist, visited, numNodes);
+    }
+
+    // store and reverse tha path
+    int *path = malloc(sizeof(int) * RoadMapNumNodes(map));
+    if (path == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    int pathLength = 0;
+    int currNode = toNode;
+    while (currNode != fromNode) {
+        path[pathLength] = currNode;
+        pathLength++;
+        currNode = prev[currNode];
+    }
+    path[pathLength] = fromNode;
+    pathLength++;
+    // reverse path
+    for (int i = 0; i < pathLength / 2; i++) {
+        int temp = path[i];
+        path[i] = path[pathLength - i - 1];
+        path[pathLength - i - 1] = temp;
+    }
+
+    // store route and legs
+    route.numLegs = pathLength - 1;
+    route.legs = malloc(sizeof(struct leg) * route.numLegs);
+    if (route.legs == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    // reverse get route
+    int indexPath = pathLength - 1;
+    struct time currTime = dist[toNode];
+    for (int i = route.numLegs - 1; i >= 0; i--) {
+        route.legs[i].fromNode = path[indexPath - 1];
+        route.legs[i].toNode = path[indexPath];
+        route.legs[i].duration =
+            RoadMapGetTravelTime(map, path[indexPath - 1], path[indexPath]);
+        // find travel time, and remove traffic light wait time
+        if (RoadMapHasTrafficLights(map, path[indexPath]) &&
+            path[indexPath - 1] != toNode) {
+            currTime = TimeAddMinutes(currTime, -TRAFFIC_LIGHT_WAIT);
+        }
+        route.legs[i].end = currTime;
+        route.legs[i].start = TimeAddMinutes(currTime, -route.legs[i].duration);
+
+        indexPath--;
+        currTime = route.legs[i].start;
+    }
+    route.start = route.legs[0].start;
+    route.end = route.legs[route.numLegs - 1].end;
+    route.totalDuration = getMinutes(route.start, route.end);
+
+    free(dist);
+    free(prev);
+    free(visited);
+    free(path);
+
+    return route;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Helper functions
+
+// task 3 find shortest path by djikstra's algorithm
+static bool findShortestPath(RoadMap map, int fromNode, int toNode, int *dist,
+                             int *prev, bool *visited) {
+    dist[fromNode] = 0;
+    prev[fromNode] = fromNode;
+    visited[fromNode] = true;
+
+    int currNode = fromNode;
+    for (int count = 0; count < RoadMapNumNodes(map) - 1; count++) {
+        // update distances
+        struct road *fromRoads =
+            malloc(sizeof(struct road) * MAX_ROADS_PER_NODE);
+        if (fromRoads == NULL) {
+            fprintf(stderr, "error: out of memory\n");
+            exit(EXIT_FAILURE);
+        }
+        int numFromRoads = RoadMapGetRoadsFrom(map, currNode, fromRoads);
+        for (int i = 0; i < numFromRoads; i++) {
+            if (visited[fromRoads[i].toNode]) {
+                continue;
+            }
+            // add traffic light wait time
+            if (RoadMapHasTrafficLights(map, fromRoads[i].toNode) &&
+                fromRoads[i].toNode != toNode) {
+                fromRoads[i].travelMinutes += TRAFFIC_LIGHT_WAIT;
+            }
+            if (dist[fromRoads[i].toNode] >
+                dist[currNode] + fromRoads[i].travelMinutes) {
+                dist[fromRoads[i].toNode] =
+                    dist[currNode] + fromRoads[i].travelMinutes;
+                prev[fromRoads[i].toNode] = currNode;
+            }
+        }
+
+        // mark node as visited
+        visited[currNode] = true;
+
+        // find next node to visit
+        currNode = minDist(dist, visited, RoadMapNumNodes(map));
+
+        // no accessible node was found
+        if (currNode == -1) {
+            free(fromRoads);
+            return false;
+        }
+
+        free(fromRoads);
+    }
+    return true;
+}
+
+// task 3 get route
+static void getRoute(RoadMap map, int fromNode, int toNode, struct time time,
+                     int *dist, int *prev, bool *visited, struct route *route) {
+    // store and reverse tha path
+    int *path = malloc(sizeof(int) * RoadMapNumNodes(map));
+    if (path == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    int pathLength = 0;
+    int currNode = toNode;
+    while (currNode != fromNode) {
+        path[pathLength] = currNode;
+        pathLength++;
+        currNode = prev[currNode];
+    }
+    path[pathLength] = fromNode;
+    pathLength++;
+    for (int i = 0; i < pathLength / 2; i++) {
+        int temp = path[i];
+        path[i] = path[pathLength - i - 1];
+        path[pathLength - i - 1] = temp;
+    }
+
+    // store route and legs
+    route->numLegs = pathLength - 1;
+    route->legs = malloc(sizeof(struct leg) * route->numLegs);
+    if (route->legs == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    struct time currTime = time;
+    for (int i = 0; i < route->numLegs; i++) {
+        route->legs[i].fromNode = path[i];
+        route->legs[i].toNode = path[i + 1];
+        route->legs[i].duration =
+            RoadMapGetTravelTime(map, path[i], path[i + 1]);
+        // find travel time, and remove traffic light wait time
+        if (RoadMapHasTrafficLights(map, path[i]) && path[i] != fromNode) {
+            route->legs[i].start = TimeAddMinutes(currTime, TRAFFIC_LIGHT_WAIT);
+            currTime = route->legs[i].start;
+        } else {
+            route->legs[i].start = currTime;
+        }
+        route->legs[i].end = TimeAddMinutes(currTime, route->legs[i].duration);
+        currTime = route->legs[i].end;
+    }
+    route->start = route->legs[0].start;
+    route->end = route->legs[route->numLegs - 1].end;
+    route->totalDuration = dist[toNode];
+    free(path);
+}
+
+// task 3 find min distance
+static int minDist(int dist[], bool visited[], int numNodes) {
+    // Initialize min value as the maximum possible time
+    int min = INT_MAX;
+    int min_index = -1;
+
+    for (int v = 0; v < numNodes; v++) {
+        if (!visited[v] && dist[v] < min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+// task 3 get travel time
+static int RoadMapGetTravelTime(RoadMap map, int fromNode, int toNode) {
+    struct road *fromRoads = malloc(sizeof(struct road) * MAX_ROADS_PER_NODE);
+    if (fromRoads == NULL) {
+        fprintf(stderr, "error: out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    int numFromRoads = RoadMapGetRoadsFrom(map, fromNode, fromRoads);
+    for (int i = 0; i < numFromRoads; i++) {
+        if (fromRoads[i].toNode == toNode) {
+            int tmp = fromRoads[i].travelMinutes;
+            free(fromRoads);
+            return tmp;
+        }
+    }
+    free(fromRoads);
+    return -1;
+}
+
+// add minutes to time, if minutes > 60, add to hour, elif mintues < 0, subtract
+struct time TimeAddMinutes(struct time t, int minutes) {
+    struct time newTime = t;
+    newTime.minute += minutes;
+    if (newTime.minute >= MINUTES_PER_HOUR) {
+        newTime.hour += newTime.minute / MINUTES_PER_HOUR;
+        newTime.minute %= MINUTES_PER_HOUR;
+    } else if (newTime.minute < 0) {
+        newTime.hour += newTime.minute / MINUTES_PER_HOUR - 1;
+        newTime.minute = MINUTES_PER_HOUR + newTime.minute % MINUTES_PER_HOUR;
+    }
+    // check if hour is over 24
+    if (newTime.hour >= 24) {
+        newTime.hour %= 24;
+    } else if (newTime.hour < 0) {
+        newTime.hour += 24;
+    }
+    return newTime;
+}
+
+// compare two times, return -1 if t1 < t2, 0 if t1 == t2, 1 if t1 > t2
+static int compareTime(struct time t1, struct time t2) {
+    if (t1.hour < t2.hour || (t1.hour == t2.hour && t1.minute < t2.minute)) {
+        return -1;
+    } else if (t1.hour == t2.hour && t1.minute == t2.minute) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+// task 4 find min distance
+static int minDistance(struct time dist[], bool visited[], int numNodes) {
+    // Initialize min value as the maximum possible time
+    struct time min = (struct time){.hour = 24, .minute = 0};
+    int min_index = -1;
+
+    for (int v = 0; v < numNodes; v++) {
+        if (!visited[v] && compareTime(dist[v], min) < 0) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+// task 4 check if road is closed
+static bool RoadMapIsClosed(RoadMap map, struct road road, struct time time) {
+    // check if there is a road closure
+    if (road.closedFrom.hour == -1) {
+        return false;
+    }
+    // check if road is closed overnight
+    if (compareTime(road.closedFrom, road.closedUntil) > 0) {
+        road.closedUntil.hour += 24;
+    }
+    // check if road is closed
+    if (compareTime(time, road.closedFrom) > 0 &&
+        compareTime(time, road.closedUntil) <= 0) {
+        return true;
+    }
+    return false;
+}
+
+// task 4 get minutes from time
+static int getMinutes(struct time start, struct time end) {
+    int minutes = 0;
+    minutes += (end.hour - start.hour) * MINUTES_PER_HOUR;
+    minutes += end.minute - start.minute;
+    return minutes;
+}
